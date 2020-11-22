9097fb0ddfd2fdb680d1ad59617e8cfd890516b0
OrginalLuminarc <74680029+OrginalLuminarc@users.noreply.github.com>
2020-11-22 17:08:55 +0100

    Nochmal

--- docs/DS_101/01_getting_started/rollDice.R
+++
@@ -1,5 +0,0 @@
-roll2 <- function(faces = 1:6, size = 2) {
-  probabilities_vector <- c(1/10, 1/10, 1/10, 1/10, 1/10, 1/2)
-  dice <- sample(faces, size, replace = TRUE, prob = probabilities_vector)
-  sum(dice)
-}
--- docs/DS_101/rollDice.R
+++
@@ -1,4 +0,0 @@
-roll2 <- function(faces = 1:6, size = 2) {
-  dice <- sample(faces, size, replace = TRUE)
-  sum(dice)
-}
